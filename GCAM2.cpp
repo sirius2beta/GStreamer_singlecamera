741d8a2fa2f9ba7f62508d726988cb6cf12b23b2
sirius2beta <92342848+sirius2beta@users.noreply.github.com>
2022-04-01 11:08:07 +0800

    Update GCAM2.cpp

--- GCAM2.cpp
+++ GCAM2.cpp
@@ -15,10 +15,10 @@ typedef struct _CustomData {
 	
 } CustomData;
 
-void heartBeat(struct mosquitto *mosq){
+void heartBeat(void* data){
   int rc = mosquitto_connect(mosq, "114.33.252.156", 1883, 10);
   String msg("HEARTBEAT CHARLIE")
-  mosquitto_publish(mosq, NULL, "test/t1", msg.length()+1, msg.toStdString().c_str(), 1, false);
+  mosquitto_publish(static_cast<struct mosquitto*>data, NULL, "test/t1", msg.length()+1, msg.toStdString().c_str(), 1, false);
 }
 
 void on_connect(struct mosquitto *mosq, void *obj, int rc) {
