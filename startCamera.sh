4a01f86cdbf786d06cecc13c2d988c9351349bcb
sirius2beta <92342848+sirius2beta@users.noreply.github.com>
2022-04-21 15:48:49 +0800

    Create startCamera.sh

---
+++ startCamera.sh
@@ -0,0 +1,5 @@
+PATH=/home/pi/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/games:/usr/games:/home/pi/.local/bin
+export PATH
+sleep 10s
+
+sudo /home/pi/GStreamer_singlecamera/GCAM_V3
