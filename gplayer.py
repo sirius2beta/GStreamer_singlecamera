0a124178f98d25284a227077dd098eb2c0e4b1e2
sirius2beta <92342848+sirius2beta@users.noreply.github.com>
2022-06-27 22:58:33 +0800

    Update gplayer.py

--- gplayer.py
+++ gplayer.py
@@ -54,9 +54,10 @@ def on_message(client, userdata, msg):
 			print('format error')
 		else:
 			gstring = 'gst-launch-1.0 -v v4l2src device=/dev/'+video 
-			gstring += ' num-buffers=-1 ! video/x-raw,format={},width={},height={} ! '.format(form,width,height)
-			gstring +=mid
-			gstring +='  ! jpegenc quality=80 ! rtpjpegpay ! udpsink host={} port={}'.format(ip, port)
+			gstring += ' num-buffers=-1 ! video/x-raw,format={},width={},height={},framerate=1/{} ! '.format(form,width,height)
+			if mid != 'nan':
+				gstring += (mid+' ! ')
+			gstring +='jpegenc quality=80 ! rtpjpegpay ! udpsink host={} port={}'.format(ip, port)
 			print(gstring)
 
 
