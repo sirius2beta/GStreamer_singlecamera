abdf158117097cdf39e5af197c0212f0404d1b93
sirius2beta <92342848+sirius2beta@users.noreply.github.com>
2022-07-06 15:38:04 +0800

    Update gplayer.py

--- gplayer.py
+++ gplayer.py
@@ -104,21 +104,12 @@ cameraformat = get_video_format()
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
-while True:
-	try:
-		rc = client.connect("114.33.252.156", 1883, 60)
-		print(rc)
-		client.loop_forever()
-	except ZeroDivisionError:
-		print("connection failed, try again")
-		time.sleep(1)
-	else:
-		break
-	
+
+client.connect("114.33.252.156", 1883, 60)
 
 # Blocking call that processes network traffic, dispatches callbacks and
 # handles reconnecting.
 # Other loop*() functions are available that give a threaded interface and a
 # manual interface.
-#client.loop_forever()
+client.loop_forever()
 
