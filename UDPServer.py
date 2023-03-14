62e66a57ab76f8ba1b379bb3478d14bf0f92858f
sirius2beta <92342848+sirius2beta@users.noreply.github.com>
2023-03-15 00:14:57 +0800

    Update UDPServer.py

--- UDPServer.py
+++ UDPServer.py
@@ -28,7 +28,7 @@ def listenLoop(ser):
 		try:
 			indata, addr = server.recvfrom(1024)
 			indata = indata.decode()
-			print(f'message from: {str(addr)}, data: {str(indata)}')
+			print(f'message from: {str(addr)}, data:')
 			# handle indata
 
 		except:
