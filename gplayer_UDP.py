8da236f066980084ba1b749a230ddf1d5e413eef
sirius2beta <92342848+sirius2beta@users.noreply.github.com>
2023-05-05 00:37:17 +0800

    Update gplayer_UDP.py

--- gplayer_UDP.py
+++ gplayer_UDP.py
@@ -103,7 +103,7 @@ def listenLoop(ser):
 				print("{} {} width={} height={} framerate={}".format(video, form, width, height, framerate))
 			else:
 				if cformat[1] == 'YUYV':
-				cformat[1] = 'YUY2'
+					cformat[1] = 'YUY2'
 
 				gstring = 'v4l2src device=/dev/'+cformat[0]
 
