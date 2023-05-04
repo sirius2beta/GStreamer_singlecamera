0c41f2260c569474f69f3d37035f85484b1bf073
sirius2beta <92342848+sirius2beta@users.noreply.github.com>
2023-05-05 00:50:38 +0800

    Update gplayer_UDP.py

--- gplayer_UDP.py
+++ gplayer_UDP.py
@@ -31,7 +31,7 @@ def aliveSignal():
 		beat = 'alive ' + BOAT_NAME
 		client.sendto(beat.encode(),(CLIENT_IP,OUT_PORT))
 		time.sleep(1)
-		//print(f"send to: {CLIENT_IP}:{OUT_PORT}")
+		#print(f"send to: {CLIENT_IP}:{OUT_PORT}")
 
 def createPipelines():
 	_pipelines = []
