0aeae963f111a38b7d3b090f75858ab39042325b
sirius2beta <92342848+sirius2beta@users.noreply.github.com>
2023-05-11 01:02:10 +0800

    Update gplayer_UDP.py

--- gplayer_UDP.py
+++ gplayer_UDP.py
@@ -138,12 +138,12 @@ def listenLoop(ser):
 						gstring += (mid+' ! ')
 					gstring +='jpegenc quality={} ! rtpjpegpay ! udpsink host={} port={}'.format(quality,ip, port)
 				elif cformat[1] == 'MJPG':
-					gstring += ' num-buffers=-1 ! video/x-raw,format=jpeg ! videoscale ! videoconvert !  video/x-raw, format=YUY2, width=160,height=120 ! '
-					if mid != 'nan':
-						gstring += (mid+' ! ')
-					gstring +='jpegparse ! rtpjpegpay ! udpsink host={} port={}'.format(ip, port)
+                                        gstring += ' num-buffers=-1 ! image/jpeg,width={},height={},framerate={}/1 ! '.format(cformat[2].split('=')[1],cformat[3].split('=')[1],cformat[4].split('=')[1])
+                                        if mid != 'nan':
+                                                gstring += (mid+' ! ')
+                                        gstring +='jpegparse ! rtpjpegpay ! udpsink host={} port={}'.format(ip, port)
 				elif cformat[1] == 'GREY':
-					gstring += ' num-buffers=-1 ! image/GREY8,width={},height={},framerate={}/1 ! '.format(cformat[2].split('=')[1],cformat[3].split('=')[1],cformat[4].split('=')[1])
+					gstring += ' num-buffers=-1 ! image/GREY8 ! videoscale ! videoconvert !  video/x-raw, format=YUY2, width=160,height=120 ! '
 					if mid != 'nan':
 						gstring += (mid+' ! ')
 					gstring +='jpegenc ! rtpjpegpay ! udpsink host={} port={}'.format(ip, port)
