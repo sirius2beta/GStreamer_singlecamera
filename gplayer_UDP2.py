84958b28f238904fb21a18636040c9c3da611cbc
sirius2beta <92342848+sirius2beta@users.noreply.github.com>
2023-05-13 01:05:33 +0800

    Update gplayer_UDP2.py

--- gplayer_UDP2.py
+++ gplayer_UDP2.py
@@ -150,10 +150,10 @@ def listenLoop(ser):
 					
 					else:
 						gstring +='jpegenc quality={} ! rtpjpegpay ! udpsink host={} port={}'.format(quality,ip, port)
-				elif cformat[1] == 'MJPG':
-                                        gstring += ' num-buffers=-1 ! image/jpeg,width={},height={},framerate={}/1 ! '.format(cformat[2].split('=')[1],cformat[3].split('=')[1],cformat[4].split('=')[1])
-                                        if encoder == 'h264':
-                                                gstring += ' jpegparse ! jpegdec ! videoconvert ! omxh264enc ! rtph264pay pt=96 config-interval=1 ! udpsink host={} port={}'.format(ip, port)
+				#elif cformat[1] == 'MJPG':
+                                 #       gstring += ' num-buffers=-1 ! image/jpeg,width={},height={},framerate={}/1 ! '.format(cformat[2].split('=')[1],cformat[3].split('=')[1],cformat[4].split('=')[1])
+                                  #      if encoder == 'h264':
+                                   #             gstring += ' jpegparse ! jpegdec ! videoconvert ! omxh264enc ! rtph264pay pt=96 config-interval=1 ! udpsink host={} port={}'.format(ip, port)
 					else:
 						gstring +='jpegparse ! rtpjpegpay ! udpsink host={} port={}'.format(ip, port)
 				else:
