2c71fc7497bb4efdba85528fb7a6489a5952ea72
sirius2beta <92342848+sirius2beta@users.noreply.github.com>
2023-05-13 01:06:27 +0800

    Update gplayer_UDP2.py

--- gplayer_UDP2.py
+++ gplayer_UDP2.py
@@ -140,6 +140,15 @@ def listenLoop(ser):
 						gstring +=' videoconvert ! omxh264enc ! rtph264pay pt=96 config-interval=1 ! udpsink host={} port={}'.format(ip, port)	
 					else:
 						gstring +='jpegenc quality={} ! rtpjpegpay ! udpsink host={} port={}'.format(quality,ip, port)
+				if cformat[1] == 'MJPG':
+					cformat[1] = 'YUY2'
+					gstring += ' num-buffers=-1 ! video/x-raw,format={},width={},height={},framerate={}/1 ! '.format(cformat[1],cformat[2].split('=')[1],cformat[3].split('=')[1],cformat[4].split('=')[1])
+					if mid != 'nan':
+						gstring += (mid+' ! ')
+					if encoder == 'h264':
+						gstring +=' videoconvert ! omxh264enc ! rtph264pay pt=96 config-interval=1 ! udpsink host={} port={}'.format(ip, port)	
+					else:
+						gstring +='jpegenc quality={} ! rtpjpegpay ! udpsink host={} port={}'.format(quality,ip, port)
 					
 				elif cformat[1] == 'GREY':
 					gstring += ' num-buffers=-1 ! video/x-raw,format=GRAY8 ! videoscale ! videoconvert ! video/x-raw, format=YUY2, width=640,height=480 ! videoconvert !  '
@@ -154,8 +163,8 @@ def listenLoop(ser):
                                  #       gstring += ' num-buffers=-1 ! image/jpeg,width={},height={},framerate={}/1 ! '.format(cformat[2].split('=')[1],cformat[3].split('=')[1],cformat[4].split('=')[1])
                                   #      if encoder == 'h264':
                                    #             gstring += ' jpegparse ! jpegdec ! videoconvert ! omxh264enc ! rtph264pay pt=96 config-interval=1 ! udpsink host={} port={}'.format(ip, port)
-					else:
-						gstring +='jpegparse ! rtpjpegpay ! udpsink host={} port={}'.format(ip, port)
+					#else:
+					#	gstring +='jpegparse ! rtpjpegpay ! udpsink host={} port={}'.format(ip, port)
 				else:
 					if cformat[1] == 'RGBP':
 						cformat[1] = 'RGB16'
