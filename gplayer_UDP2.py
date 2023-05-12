4bab9559c93458f56f3e4929eb40ff29b516180f
sirius2beta <92342848+sirius2beta@users.noreply.github.com>
2023-05-13 00:56:15 +0800

    Update gplayer_UDP2.py

--- gplayer_UDP2.py
+++ gplayer_UDP2.py
@@ -145,6 +145,7 @@ def listenLoop(ser):
                                         gstring += ' num-buffers=-1 ! image/jpeg,width={},height={},framerate={}/1 ! '.format(cformat[2].split('=')[1],cformat[3].split('=')[1],cformat[4].split('=')[1])
                                         if mid != 'nan':
                                                 gstring += (mid+' ! ')
+						
 					if encoder == 'h264':
 						gstring +=' jpegparse ! jpegdec ! videoconvert ! omxh264enc ! rtph264pay pt=96 config-interval=1 ! udpsink host={} port={}'.format(ip, port)
 					else:
