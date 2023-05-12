003a9415782abee56e47808f26b703bb670fb921
sirius2beta <92342848+sirius2beta@users.noreply.github.com>
2023-05-13 00:07:50 +0800

    Update gplayer_UDP2.py

--- gplayer_UDP2.py
+++ gplayer_UDP2.py
@@ -135,7 +135,11 @@ def listenLoop(ser):
 					gstring += ' num-buffers=-1 ! video/x-raw,format={},width={},height={},framerate={}/1 ! '.format(cformat[1],cformat[2].split('=')[1],cformat[3].split('=')[1],cformat[4].split('=')[1])
 					if mid != 'nan':
 						gstring += (mid+' ! ')
-					gstring +='jpegenc quality={} ! rtpjpegpay ! udpsink host={} port={}'.format(quality,ip, port)
+					if cformat[5] == 'h264':
+						gstring +=' videoconvert ! omxh264enc ! rtph264pay pt=96 config-interval=1 ! udpsink host={} port={}'.format(quality,ip, port)
+						
+					else:
+						gstring +='jpegenc quality={} ! rtpjpegpay ! udpsink host={} port={}'.format(quality,ip, port)
 				elif cformat[1] == 'MJPG':
                                         gstring += ' num-buffers=-1 ! image/jpeg,width={},height={},framerate={}/1 ! '.format(cformat[2].split('=')[1],cformat[3].split('=')[1],cformat[4].split('=')[1])
                                         if mid != 'nan':
